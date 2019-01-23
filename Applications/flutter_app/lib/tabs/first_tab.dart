27b1137e60040da37263652315041489934f8854
Danil Shakhov <danil.shahov@enterra-inc.com>
2019-01-23 10:38:10 +0700

    Code style.

--- Applications/flutter_app/lib/tabs/first_tab.dart
+++ Applications/flutter_app/lib/tabs/first_tab.dart
@@ -9,6 +9,7 @@ class DynamicListView extends StatefulWidget {
 
 class DynamicListViewState extends State<DynamicListView> {
   final TextEditingController eCtrl = new TextEditingController();
+
   DBService _service;
 
   Future<List<City>> _getCities() async
@@ -25,122 +26,109 @@ class DynamicListViewState extends State<DynamicListView> {
 
   @override
   Widget build(BuildContext context) {
-
-    Orientation orientation = MediaQuery
-        .of(context)
-        .orientation;
-    bool portrait = orientation == Orientation.portrait;
-
-
-    if (orientation == Orientation.portrait || orientation == Orientation.landscape) {
-      return new Scaffold(
-         appBar: AppBar(
-           leading: new Icon(Icons.edit, color: Colors.black,),
-           backgroundColor: Colors.white,
-           title: new TextField(
-              decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(15),
-              hintText: "Enter city name ..."
-            ),
-            controller: eCtrl,
-            onSubmitted: (text) async {
-              if (text != null && text.isNotEmpty) {
-                await _service.saveCity(new City(text, 0, 0));
-                eCtrl.clear();
-              }
+    return new Scaffold(
+      appBar: AppBar(
+       leading: new Icon(Icons.edit, color: Colors.black,),
+       backgroundColor: Colors.white,
+       title: new TextField(
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(15),
+            hintText: "Enter city name ..."
+          ),
+          controller: eCtrl,
+          onSubmitted: (text) async {
+            if (text != null && text.isNotEmpty) {
+              await _service.saveCity(new City(text, 0, 0));
+              eCtrl.clear();
             }
-          )
-         ),
-          body: new Center(
-            child: FutureBuilder<List<City>>(
-                future: _getCities(),
-                builder: (BuildContext context, AsyncSnapshot<List<City>> snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, index) {
-                          return new Dismissible(
-                            key: Key(snapshot.data[index].id.toString()),
-                            child: new Column(
-                                children: <Widget>[
-                                  new Row(children: <Widget>[
-                                    new Expanded(
-                                      child: new ListTile(
-                                        title: new Text('City: ${snapshot.data[index].name}'),
-                                        subtitle: new Row(
-                                          children: <Widget>[
-                                            new Text('Likes: ${snapshot.data[index].likeCount}'),
-                                            new SizedBox(width: 20),
-                                            new Text('Dislikes: ${snapshot.data[index].dislikeCount}')
-                                          ],
-                                        ),
-                                      ),
-                                      flex: 3,
-                                    ),
-                                    new Expanded(
-                                        child: new RawMaterialButton(
-                                            onPressed: () async {
-                                              await _service.updateCity(snapshot.data[index]..likeCount += 1);
-                                              setState(() {});
-                                            },
-                                            shape: new CircleBorder(),
-                                            elevation: 2.0,
-                                            fillColor: Colors.orange[100],
-                                            padding: const EdgeInsets.all(
-                                                10.0),
-                                            child: Icon(
-                                                Icons.exposure_plus_1,
-                                                color: Colors.green[300]
-                                            )
-                                        ),
-                                        flex: 1
-                                    ),
-                                    new Expanded(
-                                        child: new RawMaterialButton(
-                                            onPressed: () async {
-                                              await _service.updateCity(
-                                                  snapshot.data[index]
-                                                    ..dislikeCount += 1);
-                                              setState(() {});
-                                            },
-                                            shape: new CircleBorder(),
-                                            elevation: 2.0,
-                                            fillColor: Colors.orange[100],
-                                            padding: const EdgeInsets.all(
-                                                10.0),
-                                            child: Icon(
-                                                Icons.exposure_neg_1,
-                                                color: Colors.red[300])
-                                        ),
-                                        flex: 1
-                                    ),
-                                    //)
-                                  ]),
-                                  new Divider(height: 5)
-                                ]
-                            ),
-                            onDismissed: (direction) async
-                            {
-                              await _service.deleteCity(
-                                  snapshot.data[index]);
-                            },
-                            background: Container(color: Colors.red[300]),
-                          );
-                        },
-                        scrollDirection: Axis.vertical
-                    );
-                  }
-                  else if (snapshot.data == null) {
-                    return new Text("No Data found");
-                  }
-                  return new Container(
-                    alignment: AlignmentDirectional.center,
-                    child: new CircularProgressIndicator(),);
-                }
-            )
+          }
         )
-      );
-    }
+      ),
+      body: new Center(
+        child: FutureBuilder<List<City>>(
+          future: _getCities(),
+          builder: (BuildContext context, AsyncSnapshot<List<City>> snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return new Dismissible(
+                    key: Key(snapshot.data[index].id.toString()),
+                    child: new Column(
+                      children: <Widget>[
+                        new Row(children: <Widget>[
+                          new Expanded(
+                            child: new ListTile(
+                              title: new Text('City: ${snapshot.data[index].name}'),
+                              subtitle: new Row(
+                                children: <Widget>[
+                                  new Text('Likes: ${snapshot.data[index].likeCount}'),
+                                  new SizedBox(width: 20),
+                                  new Text('Dislikes: ${snapshot.data[index].dislikeCount}')
+                                ],
+                              ),
+                            ),
+                            flex: 3,
+                          ),
+                          new Expanded(
+                            child: new RawMaterialButton(
+                              onPressed: () async {
+                                await _service.updateCity(snapshot.data[index]..likeCount += 1);
+                                setState(() {});
+                              },
+                              shape: new CircleBorder(),
+                              elevation: 2.0,
+                              fillColor: Colors.orange[100],
+                              padding: const EdgeInsets.all(10.0),
+                              child: Icon(
+                                Icons.exposure_plus_1,
+                                color: Colors.green[300]
+                              )
+                            ),
+                            flex: 1
+                          ),
+                          new Expanded(
+                            child: new RawMaterialButton(
+                              onPressed: () async {
+                                await _service.updateCity(snapshot.data[index]..dislikeCount += 1);
+                                setState(() {});
+                              },
+                              shape: new CircleBorder(),
+                              elevation: 2.0,
+                              fillColor: Colors.orange[100],
+                              padding: const EdgeInsets.all(10.0),
+                              child: Icon(
+                                Icons.exposure_neg_1,
+                                color: Colors.red[300])
+                            ),
+                            flex: 1
+                          ),
+                          //)
+                        ]),
+                        new Divider(height: 5)
+                      ]
+                    ),
+                    onDismissed: (direction) async
+                    {
+                      await _service.deleteCity(snapshot.data[index]);
+                    },
+                    background: Container(color: Colors.red[300]),
+                  );
+                },
+                scrollDirection: Axis.vertical
+              );
+            }
+            else if (snapshot.data == null) {
+              return new Text("No Data found");
+            }
+            return new Container(
+              alignment: AlignmentDirectional.center,
+              child: new CircularProgressIndicator()
+            );
+          }
+        )
+      )
+    );
   }
 }
 
--- Applications/flutter_app/lib/tabs/second_tab.dart
+++ Applications/flutter_app/lib/tabs/second_tab.dart
@@ -40,117 +40,99 @@ class AddContactTabState extends State<AddContactTab>
 
   @override
   Widget build(BuildContext context) {
-    Orientation orientation = MediaQuery
-        .of(context)
-        .orientation;
 
-    if(orientation == Orientation.portrait || orientation == Orientation.landscape) {
-      // TODO: implement build
-      return new Scaffold(
-          body: new Center(
-              child: FutureBuilder<List<Contact>>(
-                  future: _getContacts(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<Contact>> snapshot) {
-                    if (snapshot.hasData) {
-                      return new ListView.builder(
-                          itemCount: snapshot.data.length,
-                          itemBuilder: (context, index) {
-                            return new Slidable(
-                              delegate: new SlidableDrawerDelegate(),
-                              actionExtentRatio: 0.25,
-                              child: new Container(
-                                  decoration: new BoxDecoration(
-                                      color: _getColor(
-                                          snapshot.data[index].favoriteColor)
-                                  ),
-                                  child: new Column(
-                                      children: <Widget>[
-                                        new ListTile(
-                                          leading: SizedBox(
-                                            child: new RawMaterialButton(
-                                                onPressed: () =>
-                                                    launch('tel://${snapshot
-                                                        .data[index].phone}'),
-                                                shape: new CircleBorder(),
-                                                elevation: 2.0,
-                                                fillColor: Colors.orange[100],
-                                                padding: const EdgeInsets.all(
-                                                    10.0),
-                                                child: Icon(Icons.phone,
-                                                    color: Colors.blue[300])
-                                            ),
-                                            width: 50.0,
-                                            height: 50.0,
-                                          ),
-                                          title: new Text(
-                                              snapshot.data[index].name,
-                                              style: new TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 18.0)),
-                                          subtitle: new Row(
-                                              children: <Widget>[
-                                                new Text('Email: ${snapshot
-                                                    .data[index].email}',
-                                                    style: new TextStyle(
-                                                        fontWeight: FontWeight
-                                                            .bold,
-                                                        fontSize: 14.0)),
-                                                new SizedBox(width: 20),
-                                                new Text('Phone:  ${snapshot
-                                                    .data[index].phone}',
-                                                    style: new TextStyle(
-                                                        fontWeight: FontWeight
-                                                            .bold,
-                                                        fontSize: 14.0)),
-                                              ]
-                                          ),
-                                        ),
-                                        new Divider(height: 3)
-                                      ]
-                                  )
+    return new Scaffold(
+      body: new Center(
+        child: FutureBuilder<List<Contact>>(
+          future: _getContacts(),
+          builder: (BuildContext context,AsyncSnapshot<List<Contact>> snapshot) {
+            if (snapshot.hasData) {
+              return new ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return new Slidable(
+                    delegate: new SlidableDrawerDelegate(),
+                    actionExtentRatio: 0.25,
+                    child: new Container(
+                      decoration: new BoxDecoration(
+                        color: _getColor(snapshot.data[index].favoriteColor)
+                      ),
+                      child: new Column(
+                        children: <Widget>[
+                          new ListTile(
+                            leading: SizedBox(
+                            child: new RawMaterialButton(
+                            onPressed: () =>
+                              launch('tel://${snapshot.data[index].phone}'),
+                              shape: new CircleBorder(),
+                              elevation: 2.0,
+                              fillColor: Colors.orange[100],
+                              padding: const EdgeInsets.all(10.0),
+                              child: Icon(Icons.phone,
+                              color: Colors.blue[300])
+                            ),
+                            width: 50.0,
+                            height: 50.0,
+                          ),
+                          title: new Text(snapshot.data[index].name,
+                            style: new TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0)),
+                              subtitle: new Row(
+                                children: <Widget>[
+                                  new Text('Email: ${snapshot.data[index].email}',
+                                    style: new TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14.0)),
+                                      new SizedBox(width: 20),
+                                      new Text('Phone:  ${snapshot.data[index].phone}',
+                                        style: new TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14.0)),
+                                ]
                               ),
-                              actions: <Widget>[
-                                new IconSlideAction(
-                                  caption: 'Copy',
-                                  color: Colors.blue,
-                                  icon: Icons.filter_none,
-                                  onTap: () async {
-                                    await _service.saveContact(
-                                        snapshot.data[index]
-                                          ..name += '-Copy');
-                                    setState(() {});
-                                  },
-                                ),
-                              ],
-                              secondaryActions: <Widget>[
-                                new IconSlideAction(
-                                  caption: 'Delete',
-                                  color: Colors.red,
-                                  icon: Icons.delete_forever,
-                                  onTap: () async {
-                                    await _service.deleteContact(
-                                        snapshot.data[index].id);
-                                    setState(() {});
-                                  },
-                                ),
-                              ],
-                            );
-                          }
-                      );
-                    }
-                    else if (snapshot.hasError) {
-                      print(snapshot.error.toString());
-                      return new Text("No Data found");
-                    }
-                    return new Container(alignment: AlignmentDirectional.center,
-                      child: new CircularProgressIndicator(),);
+                            ),
+                            new Divider(height: 3)
+                          ]
+                        )
+                      ),
+                      actions: <Widget>[
+                        new IconSlideAction(
+                          caption: 'Copy',
+                          color: Colors.blue,
+                          icon: Icons.filter_none,
+                          onTap: () async {
+                            await _service.saveContact(snapshot.data[index]..name += '-Copy');
+                                  setState(() {});
+                          },
+                        ),
+                      ],
+                      secondaryActions: <Widget>[
+                        new IconSlideAction(
+                          caption: 'Delete',
+                          color: Colors.red,
+                          icon: Icons.delete_forever,
+                          onTap: () async {
+                            await _service.deleteContact(snapshot.data[index].id);
+                            setState(() {});
+                          },
+                        ),
+                      ],
+                    );
                   }
-              )
+                );
+              }
+              else if (snapshot.hasError) {
+                return new Text("No Data found");
+              }
+              return new Container(
+                alignment: AlignmentDirectional.center,
+                child: new CircularProgressIndicator()
+              );
+            }
           )
-      );
-    }
-    return Text("Landscape");
+      )
+    );
   }
 }
 
